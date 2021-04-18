e4bc34f1c58fb8a0e0c8acb8f9b6fa0788e2bcb6
JakeL1001 <jakelyell@iinet.net.au>
2021-04-18 21:01:27 +0800

    login page update
    removed bootstrap css, imported in page instead

---
+++ Pages/js/login-page.js
@@ -0,0 +1,3 @@
+// Currently not working
+// turns out login systems are quite hard and i will not be attempting at this point in time lmao
+// How do we store each user's login information and progress in the lessons????
