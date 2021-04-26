4e5fc4a851c8b2ebaa241310c20985947730877c
JakeL1001 <jakelyell@iinet.net.au>
2021-04-26 22:06:02 +0800

    Flask Update
    Currently, planning to shift all pages over to flask system. started to do that

---
+++ pokerapp/templates/js/login-page.js
@@ -0,0 +1,4 @@
+// Currently not working
+// turns out login systems are quite hard and i will not be attempting at this point in time lmao
+// How do we store each user's login information and progress in the lessons????
+// Think its some flask python stuff (i really wanna swear in these comments but dont know if they show up in the logs we have to submit lol)
