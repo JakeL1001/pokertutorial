31367e573e9c01331aa1feeea7bfb90cb5b37180
Kanealex <72069221+Kanealex@users.noreply.github.com>
2021-05-06 15:37:50 +0800

    clean up

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jakel\OneDrive\Documents\GitHub\pokertutorial\venv
+set VIRTUAL_ENV=C:\Users\kaneb.DESKTOP-A94UFDN\OneDrive\Documents\pokertutorial\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
