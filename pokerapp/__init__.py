73801bc44949018d6da99a7776ee7970d6cc2e49
JakeL1001 <jakelyell@iinet.net.au>
2021-05-06 14:16:46 +0800

    Login almost finished
    Login not quite funcitonal. uploading to combine with main

--- pokerapp/__init__.py
+++ pokerapp/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 pokerpack = Flask(__name__)
 pokerpack.config.from_object(Config)
+db = SQLAlchemy(pokerpack)
+migrate = Migrate(pokerpack, db)
+login = LoginManager(pokerpack)
+login.login_view = "login"
 
-from pokerapp import routes
+from pokerapp import routes, models
