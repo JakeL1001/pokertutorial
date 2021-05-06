30d3bddff1ab761946304b631a1dbc20e95f0179
JakeL1001 <jakelyell@iinet.net.au>
2021-05-06 15:11:34 +0800

    requirements.txt
    run pip install -r requirements.txt on local machine to install dependencies

--- pokerapp/routes.py
+++ pokerapp/routes.py
@@ -1,6 +1,6 @@
-from pokerapp import pokerpack
+from pokerapp import pokerpack, db
 from flask import render_template, flash, redirect, url_for, request
-from pokerapp.forms import LoginForm
+from pokerapp.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from pokerapp.models import User
 from werkzeug.urls import url_parse
@@ -40,6 +40,20 @@ def login():
     return render_template("login.html", title="Sign In", form=form)
 
 @pokerpack.route("/logout")
-def logour():
+def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@pokerpack.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations! You are now registered!")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
