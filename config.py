f49aba7c18be9a0111c1ab4b3b350409a7b520e9
JakeL1001 <jakelyell@iinet.net.au>
2021-05-14 14:33:21 +0800

    Cleanup of code
    Added pass or fail colours for quiz results. Edited other code

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "poggers"
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "No-one-will-know32521"
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'pokerdb.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
