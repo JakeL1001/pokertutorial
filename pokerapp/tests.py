318ddc857f0142e256e91ea588ad245c4226edbc
JLhyun <79490049+JLhyun@users.noreply.github.com>
2021-05-17 14:09:15 +0800

    added lesson login test
    updated selenium

--- pokerapp/tests.py
+++ pokerapp/tests.py
@@ -1,3 +1,4 @@
+from os import access
 import selenium
 from selenium import webdriver
 #from selenium.webdriver import Firefox, Chrome, Edge
@@ -7,14 +8,37 @@ import time
 chrome_PATH = "chromedriver.exe" #for now just chrome web driver but for firefox e.g. similar setup . currently chrome v 90
 driver = webdriver.Chrome(chrome_PATH) 
 
-driver.get("http://localhost:5000/login")
-username = driver.find_element_by_id("Username")
-password = driver.find_element_by_id("Password")
+def test_login():
+    driver.get("http://localhost:5000/login")   
+    #works from here
+    username = driver.find_element_by_id("username")
+    password = driver.find_element_by_id("password")
 
-username.send_keys("jord")
-password.send_keys("jord")
+    username.send_keys("jord")
+    password.send_keys("jord")
 
-link = driver.find_element_by_link_text("Sign In")
-link.click()
-time.sleep(10)
-driver.quit()
+    driver.find_element_by_id("submit").click()
+    return
+
+def access_lessons(): #testing lesson access with login/out
+    test_login()
+    driver.find_element_by_link_text("Begin Learning").click()
+    driver.get("http://localhost:5000/lesson1")
+
+
+
+    driver.find_element_by_link_text("Logout").click()
+    driver.get("http://localhost:5000/Home")
+    driver.find_element_by_link_text("Begin Learning").click()
+    driver.get("http://localhost:5000/lesson1")
+
+    return
+
+
+
+def main():
+    access_lessons()
+
+    return
+
+main()
